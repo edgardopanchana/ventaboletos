a63bd689f66b18564f78cc868e7b8550ee78f2b1
edgardopanchana <edgardo.panchana@uleam.edu.ec>
2025-09-29 15:27:51 -0500

    cuerpo de la clase vuelo

--- vuelo.ts
+++ vuelo.ts
@@ -1,3 +1,12 @@
 class Vuelo{
+
+    numeroVuelo: string;
+    origen: string
+
+    constructor(numeroVuelo: string, origen: string){
+
+        this.numeroVuelo = numeroVuelo;
+        this.origen = origen;
+    }   
     
 }
